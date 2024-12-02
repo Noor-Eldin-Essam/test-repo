f8f84533f39068a565f46371993a69df1fc8225b
Noor-Eldin-Essam <Noor-Eldin-Essam@users.noreply.github.com>
2024-12-02 13:33:21 +0000

    deploy: 712cc904415b0eb6920b0dd2e7fc79299c17c749

---
+++ auth.js
@@ -0,0 +1,7 @@
+function verify(token_id,cookie_name){let queryString=window.location.search;let urlParams=new URLSearchParams(queryString);let token=urlParams.get('token')
+    if(!isRunningStandalone()){if(token!=`${token_id}`&&getCookie(`${cookie_name}`)===0){var splitUrl=location.pathname.split("/");if(document.location.pathname.includes("product_landingpage_eliquis")){if(splitUrl[1]==="en"){window.location.replace(window.location.origin+"/en/scanqr_iq.html");return}else{window.location.replace(window.location.origin+"/scanqr_iq.html");return}}else{if(splitUrl[1]==="en"){window.location.replace(window.location.origin+"/en/scanqr.html");return}else{window.location.replace(window.location.origin+"/scanqr.html");return}}}else{window.history.pushState("","",`${location.pathname}`);setCookie(`${cookie_name}`,1,10)}}}
+    function setCookie(cname,cvalue,exdays){const d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));let expires="expires="+d.toUTCString();document.cookie=cname+"="+cvalue+";"+expires+";path=/"}
+    function getCookie(cname){let name=cname+"=";let decodedCookie=decodeURIComponent(document.cookie);let ca=decodedCookie.split(';');for(let i=0;i<ca.length;i++){let c=ca[i];while(c.charAt(0)==' '){c=c.substring(1)}
+    if(c.indexOf(name)==0){return c.substring(name.length,c.length)}}
+    return 0}
+    function isRunningStandalone(){return navigator.standalone||(window.matchMedia('(display-mode: standalone)').matches)}
