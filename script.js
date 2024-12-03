eb40fef0bc84678bfeafa6cd74495b09bb1a38cc
Noor-Eldin-Essam <Noor-Eldin-Essam@users.noreply.github.com>
2024-12-03 10:09:44 +0000

    deploy: 6a25c3612f447fda5f6a42285216825ec1207011

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+// script.js
 import { validTokens } from './tokens.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(validTokens);
-    
     const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
+    const token = urlParams.get('token');  // Get the 'token' from the URL parameters
 
-    if (validTokens.includes(token)) {
-        // Show protected content
-        document.getElementById('content').style.display = 'block';
-    } else {
-        // Show error and redirect
-        document.getElementById('error').style.display = 'block';
-        setTimeout(() => {
-            window.location.href = '/unauthorized.html';
-        }, 3000); // Redirect after 3 seconds
-    }
+    // If there's no token in the URL, show an error and exit
+    // if (!token) {
+    //     showError();
+    //     return;
+    // }
+    console.log(token);
+    
+    // Hash the token from the URL using SHA-256 and convert to Base64
+    const hashedToken = CryptoJS.SHA256(token).toString(CryptoJS.enc.Base64);
+    console.log(hashedToken);
+    console.log(validTokens[0]);
+    
+    
+
+    // Check if the hashed token is in the validTokens array
+//     if (validTokens.includes(hashedToken)) {
+//         // Show protected content
+//         document.getElementById('content').style.display = 'block';
+//     } else {
+//         // Show error and redirect
+//         showError();
+//     }
 });
+
+function showError() {
+    // Show error and redirect
+    document.getElementById('error').style.display = 'block';
+    setTimeout(() => {
+        window.location.href = '/unauthorized.html';  // Redirect to unauthorized page
+    }, 3000);  // Redirect after 3 seconds
+}
---
+++ tokens.js
@@ -0,0 +1,1 @@
+export const validTokens = ['pbQy7gMHvn+iOqAEYfVO7jS6nUUlG1UEVn03qNoznf8='];
