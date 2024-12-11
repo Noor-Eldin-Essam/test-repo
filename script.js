565c252273471470f0ffa2b5dc41b9c1acf61bc6
Noor-Eldin-Essam <Noor-Eldin-Essam@users.noreply.github.com>
2024-12-11 11:50:55 +0000

    deploy: ee2ed972fbaf09190feb72bc24f30b72a0de38f5

---
+++ script.js
@@ -0,0 +1,34 @@
+// script.js
+import { validTokens } from './tokens.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');  // Get the 'token' from the URL parameters
+
+    // If there's no token in the URL, show an error and exit
+    if (!token) {
+        showError();
+        return;
+    }
+    console.log(token);
+    console.log(validTokens[0]);
+    
+    
+
+    // Check if the hashed token is in the validTokens array
+    if (token === validTokens[0]) {
+        // Show protected content
+        document.getElementById('content').style.display = 'block';
+    } else {
+        // Show error and redirect
+        showError();
+    }
+});
+
+function showError() {
+    // Show error and redirect
+    document.getElementById('error').style.display = 'block';
+    setTimeout(() => {
+        window.location.href = '/unauthorized.html';  // Redirect to unauthorized page
+    }, 3000);  // Redirect after 3 seconds
+}
