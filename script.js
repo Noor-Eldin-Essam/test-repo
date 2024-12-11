a16420631db94ee7383224c5e50e8a86e0689fa3
Noor-Eldin-Essam <Noor-Eldin-Essam@users.noreply.github.com>
2024-12-11 10:28:29 +0000

    deploy: 23a37a2a35c20ec12452f3b17b2c632524539575

--- script.js
+++ script.js
@@ -6,33 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const token = urlParams.get('token');  // Get the 'token' from the URL parameters
 
     // If there's no token in the URL, show an error and exit
-    // if (!token) {
-    //     showError();
-    //     return;
-    // }
+    if (!token) {
+        showError();
+        return;
+    }
     console.log(token);
     
-    // Hash the token from the URL using SHA-256 and convert to Base64
-    // const hashedToken = CryptoJS.SHA256(token).toString(CryptoJS.enc.Base64);
-    // console.log(hashedToken);
+    console.log(hashedToken);
     console.log(validTokens[0]);
     
     
 
     // Check if the hashed token is in the validTokens array
-//     if (validTokens.includes(hashedToken)) {
-//         // Show protected content
-//         document.getElementById('content').style.display = 'block';
-//     } else {
-//         // Show error and redirect
-//         showError();
-//     }
+    if (token === validTokens[0]) {
+        // Show protected content
+        document.getElementById('content').style.display = 'block';
+    } else {
+        // Show error and redirect
+        showError();
+    }
 });
 
-// function showError() {
-//     // Show error and redirect
-//     document.getElementById('error').style.display = 'block';
-//     setTimeout(() => {
-//         window.location.href = '/unauthorized.html';  // Redirect to unauthorized page
-//     }, 3000);  // Redirect after 3 seconds
-// }
+function showError() {
+    // Show error and redirect
+    document.getElementById('error').style.display = 'block';
+    setTimeout(() => {
+        window.location.href = '/unauthorized.html';  // Redirect to unauthorized page
+    }, 3000);  // Redirect after 3 seconds
+}
